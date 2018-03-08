4882b0e7e1b0a76e7f3216ad4ef6630cb4e248c9
MandarSy <32954876+ArmandSy@users.noreply.github.com>
2018-03-08 18:07:33 +0100

    Newton généralisée
    Méthode de Newton généralisée pour n'importe quel polynôme.
    
    La méthode pour trouver les racines en amont est celle de Laguerre.

--- Newton.java
+++ Newton.java
@@ -2,7 +2,11 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import org.apache.commons.math3.complex.Complex;
+
+import flanagan.complex.ComplexPoly;
+import flanagan.complex.Complex;
+
+
 
 
 import javax.swing.JFrame;
@@ -20,15 +24,19 @@ public class Newton extends JComponent
 	public static final int HAUTEUR = 720; // '' hauteur de la fenetre
 	public static final int ITERATIONS = 500; // Définition du nombre d'itérations
 	public static final float ECHELLE = 300; // Definition de l'echelle de grandeur
-	public static final Complex r1 = new Complex(1,0); // Racine réelle du polynome de Newton classique
-	public static final Complex r2 = new Complex(-0.5,-Math.sqrt(3)/2); // Racine complexe du polynome de Newton classique
-	public static final Complex r3 = new Complex(-0.5,Math.sqrt(3)/2); // Racine complexe conjuguée
 	
 	public static final double epsilon = (double)0.1; //plus epsilon est petit, plus le calcul est précis, et le temps de calcul évolue de manière exponentielle. Tester avec de petites valeurs au début
 	public static final double alpha = (double)1;
 	
+	public static double [] coefficients1 = {2,-2,0,1}; // on définit les coefficients du polynome
 	
-	static Complex [] roots = {r1,r2,r3};
+	//public static PolynomialFunction P1 = new PolynomialFunction(coefficients1); test de la lib d'apache polynomiale
+	//public static PolynomialFunction P2 = new PolynomialFunction(coefficients2); // on construit les polynomes
+	
+	public static ComplexPoly P1 = new ComplexPoly(coefficients1);
+	public static ComplexPoly P2 = P1.nthDerivative(1);
+	
+	public static final Complex[] roots = P1.roots();
 	
 	private BufferedImage buffer; // Image tampon
 	
@@ -45,7 +53,8 @@ public class Newton extends JComponent
 		frame.pack(); // Permet d'adapter la taille de la fenetre suivant les cas de figures
 		frame.setVisible(true); // Pour que la fenetre soit visible
 
-
+		//P2 = P2.multiply(P1); test de la lib polynomiale d'apache
+		//System.out.println("le degré est :" + P2.degree());
 	}
 
 	@Override 
@@ -72,41 +81,33 @@ public class Newton extends JComponent
 		
 		for(; n < ITERATIONS; n++)
 		{
-			z = (((z.pow(3)).multiply(2)).add(1)).divide((z.pow(2)).multiply(3)); // z = 2z^3 + 1 / 3z^2
+			//z = P1.value(z)/P1.DerivativeStructure(z);
+			//z = ((z.pow(3).times(2)).plus(1)).over((z.pow(2)).times(3));
+			z = z.minus((P1.evaluate(z)).times(alpha).over(P2.evaluate(z)));
 			for(Complex item : roots)
 			{
-				if(Math.abs(z.getReal() - item.getReal()) < epsilon && Math.abs(z.getImaginary() - item.getImaginary()) < epsilon)
-						{
-							if(item.getImaginary()==0)
-							{
-								return Color.HSBtoRGB((float)Math.sqrt((double)40*n/ITERATIONS) , 0.5f, 1); // ce point converge vers la racine réelle
-								//return 0xFF0000FF;
-							}
-							else if(item.getImaginary()<0)
-							{
-								return Color.HSBtoRGB((float)Math.sqrt((double)40*n/ITERATIONS) , 0.5f, 1); // ce point converge vers la 1ere racine complexe
-								//return 0xFFFF0000;
-							}
-							else if(item.getImaginary()>0)
-							{
-								return Color.HSBtoRGB((float)Math.sqrt((double)40*n/ITERATIONS) , 0.5f, 1); // ce point converge vers la racine complexe conjuguée
-								//return 0xFF00FF00;
-							}
-						}
+			if(Math.abs(z.getReal() - item.getReal()) < epsilon && Math.abs(z.getImag() - item.getImag()) < epsilon)
+			{
+				if(item.getImag()==0)
+				{
+					return Color.HSBtoRGB((float)Math.sqrt((double)30*n/ITERATIONS) , 0.5f, 1); // ce point converge vers la racine réelle
+					//return 0xFF0000FF;
+				}
+				else if(item.getImag()<0)
+				{
+					return Color.HSBtoRGB((float)Math.sqrt((double)30*n/ITERATIONS) , 0.5f, 1); // ce point converge vers la 1ere racine complexe
+					//return 0xFFFF0000;
+				}
+				else if(item.getImag()>0)
+				{
+					return Color.HSBtoRGB((float)Math.sqrt((double)30*n/ITERATIONS) , 0.5f, 1); // ce point converge vers la racine complexe conjuguée
+					//return 0xFF00FF00;
+				}
+			}
 			}
 		}
 		return 0xFFFFFFFF;
 }
-	
-	public Complex poly(Complex z)
-	{
-		return (z.pow(3)).subtract(1);
-	}
-	
-	public Complex deriv(Complex z)
-	{
-		return (z.pow(2)).multiply(3);
-	}
 	
 	@Override
 	public void paint(Graphics g)
@@ -118,3 +119,4 @@ public class Newton extends JComponent
 	// HSBtoRGB(float hue, float saturation, float brightness)
 	//Converts the components of a color, as specified by the HSB model, to an equivalent set of values for the default RGB model.
 }
+
